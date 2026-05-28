e15b5861e6e744fdb0ce3192d0a51e675bf9fd8f
Amit Singh <amitfr489@gmail.com>
2026-05-28 14:15:11 +0530

    Fix: Update next.config.ts to set output to 'standalone'

--- ikigaifarm/next.config.ts
+++ ikigaifarm/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // output: "export",
-  assetPrefix: 'https://ikigaifarm.org',
+   output: 'standalone',
 };
 
 export default nextConfig;
